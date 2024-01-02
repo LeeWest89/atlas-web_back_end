0f35bdadc1afc423f785bae3c73d7c6651a9faf5
LeeWest89 <lee.west@atlasschool.com>
2024-01-02 14:01:03 -0800

    going back to an old idea, thought i had to use const but other resources are telling me to use this

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
 export default function getNeighborhoodsList() {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
   };
 }
