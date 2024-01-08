a96627426f85ae8e9130ae2b6b21240b8db63f2f
LeeWest89 <lee.west@atlasschool.com>
2024-01-08 12:40:02 +0000

    read about setInt8 sounds like it might fix my problem

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-  const int8A = new Int8Array(new ArrayBuffer(length));
-  int8A.fill(0);
-  int8A[position] = value;
-  return new DataView(new ArrayBuffer(length));
+  const data = new DataView(new ArrayBuffer(length));
+  data.setInt8(position, value);
+  return data;
 }
