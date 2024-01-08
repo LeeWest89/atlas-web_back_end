e1df5422d023a8b17f2effa678f7102ac84c7428
LeeWest89 <lee.west@atlasschool.com>
2024-01-08 22:18:31 +0000

    i think the error is in the first if statement, try a different format

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || (typeof startString !== 'string' && typeof startString === 'object')) {
+  if (!startString.length || !startString) {
     return '';
   }
-  let values = Array.from(set)
+  const values = Array.from(set)
     .filter((value) => value && value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
-  values = values || [];
-  return values.length > 0 ? values.join('-') : '';
+    .map((value) => value.slice(startString.length))
+    .join('-');
+  return values.length > 0 ? values : '';
 }
