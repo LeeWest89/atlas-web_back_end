e232859920e2fc432f9ef0a5a071e7546302d650
LeeWest89 <lee.west@atlasschool.com>
2024-01-08 21:11:21 +0000

    fixing linter error

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -5,5 +5,5 @@ export default function cleanSet(set, startString) {
   const values = Array.from(set)
     .filter((value) => value && value.startsWith(startString))
     .map((value) => value.slice(startString.length));
-    return values.length > 0 ? values.join('-') : '';
+  return values.length > 0 ? values.join('-') : '';
 }
