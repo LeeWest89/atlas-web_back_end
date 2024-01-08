f5980a197270510515e2469ce70909438d6ec893
LeeWest89 <lee.west@atlasschool.com>
2024-01-08 22:43:18 +0000

    check to see if the startString checks are causing an issue

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString.length || !startString) {
+  if (!startString || !startString.length) {
     return '';
   }
   const values = Array.from(set)
