9e40bdc3de6bfd88d9a14cc88fd4bc8bf6173893
LeeWest89 <lee.west@atlasschool.com>
2024-01-08 21:30:43 +0000

    if startString is not empty now it should return ''

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,9 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0 || typeof startString !== 'string') {
     return '';
   }
-  const values = Array.from(set)
+  let values = Array.from(set)
     .filter((value) => value && value.startsWith(startString))
     .map((value) => value.slice(startString.length));
+  values = values || [];
   return values.length > 0 ? values.join('-') : '';
 }
