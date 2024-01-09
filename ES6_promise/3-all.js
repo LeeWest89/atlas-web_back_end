6d9f3e68dbc83a9454d71ac414c5a9d926f6464f
LeeWest89 <lee.west@atlasschool.com>
2024-01-09 18:04:24 +0000

    learned that Promise.all is used for collective resolution

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,1 +1,11 @@
-export default function
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
