66b20e502cd28fd6ea703594071064bec5f4fcd2
LeeWest89 <lee.west@atlasschool.com>
2024-01-09 18:48:48 +0000

    Seems to be testing well, I think there is away to do this with async if this fails

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,1 +1,16 @@
-export default function
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName).then((response) => ({
+    status: 'resolved',
+    value: response,
+  }));
+
+  const photo = uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error,
+  }));
+
+  return Promise.all([user, photo]);
+}
