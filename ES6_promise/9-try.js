bb06d2963b0f7654f29badba4e0f50b0e495ab69
LeeWest89 <lee.west@atlasschool.com>
2024-01-09 20:10:19 +0000

    not to bad, learned the synatax for finally don't know if i prefer that way or with the queue.push above the return

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,1 +1,14 @@
-export default function
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let result = 0;
+
+  try {
+    result = mathFunction();
+  } catch (error) {
+    result = `${error.name}: ${error.message}`;
+  } finally {
+    queue.push(result);
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
