95ca9f62fcbf93684b20454b95b87fd7b0799d2f
LeeWest89 <6683@holbertonstudents.com>
2024-04-29 19:30:44 +0000

    converting float to str in 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""takes a float n as argument and returns the str representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """changes a float to a string representation
+    """
+    return (str(n))
