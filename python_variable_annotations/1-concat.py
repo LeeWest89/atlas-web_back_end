28047ef91cc21ba4a665268910416dc58dbfb5af
LeeWest89 <6683@holbertonstudents.com>
2024-04-29 19:00:58 +0000

    fixing documentation again, really need to remember to check

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-""" takes a string str1 and a string str2 as arguments and returns a concatenated string
+"""takes a str1 and a str2 as arguments and returns a concatenated string
 """
 
 
 def concat(str1: str, str2: str) -> str:
+    """combines the two strings
+    """
     return (str1 + str2)
