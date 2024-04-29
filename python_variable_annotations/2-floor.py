0dea2eca17df1a9a0962e756a778ef35e03ccf78
LeeWest89 <6683@holbertonstudents.com>
2024-04-29 19:14:09 +0000

    Kept getting false, relized I needed to import math to get true

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""takes a float n as argument and returns the floor of the float
+"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """ returns the floor of the float
+    """
+    return (math.floor(n))
