0f44f3af5fafc9effc9bf00d0440332274f3236a
LeeWest89 <6683@holbertonstudents.com>
2024-04-30 19:38:46 +0000

    didn't realize I need to use typing.Generator, ituses 3 types, Yeild, Send, and Return

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@ loop that yeilds a random number between 0 and 10
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> typing.Generator[float, None, None]:
     """
     Waits 1 second between each loop
     """
