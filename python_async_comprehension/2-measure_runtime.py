d55d6eef8f7befb809f0e4408784ce05e347d84d
LeeWest89 <6683@holbertonstudents.com>
2024-04-30 20:21:03 +0000

    Taking the Measure of async_comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""
+Measuring the runtime of 4 async_comprehensions running in parallel
+"""
+
+
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Determining the measurement
+    """
+    start = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end = asyncio.get_event_loop().time()
+    measurement = end - start
+    return (measurement)
