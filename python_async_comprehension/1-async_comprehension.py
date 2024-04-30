849d5b13509b6deed507d807337bfe2d583d9913
LeeWest89 <6683@holbertonstudents.com>
2024-04-30 20:02:49 +0000

    Returning a list of floats, iterating asynchronously

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+Loop that yeilds a random number between 0 and 10 and creates a list
+"""
+
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    return a list of floats, iterating asynchronously
+    """
+    return ([delay async for delay in async_generator()])
