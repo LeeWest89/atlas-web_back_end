38e1a724008cd250e092c1a7423589e745f0aed6
LeeWest89 <6683@holbertonstudents.com>
2024-04-30 04:43:30 +0000

    measure average delay time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+Measures the average delay
+"""
+
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the start and end times to find an average
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return (total_time / n)
