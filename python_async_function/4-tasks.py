7bf143669166a79f8b4bfe60a8284917e210a12d
LeeWest89 <6683@holbertonstudents.com>
2024-04-30 05:08:17 +0000

    this seems to easy, I have to be missing something

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Making function that creates multiple delays using wait_random
+"""
+
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ calling wait-random n number of times then sort the return
+    """
+    delays = [wait_random(max_delay) for i in range(n)]
+    delay_list = await asyncio.gather(*delays)
+
+    return (sorted(delay_list))
