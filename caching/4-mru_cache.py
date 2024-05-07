a9f186f434bf482710d7e4b04cf5fdabdb195ae5
LeeWest89 <6683@holbertonstudents.com>
2024-05-07 22:07:18 +0000

    Very similar to LRU but switch what to pop from 0 to -1

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""Createing class LRUCache"""
+
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Using MRU to create a cache system"""
+    def __init__(self):
+        """Starting Cache"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Adding keys or overwriting items that are the most used"""
+        if key is None or item is None:
+            return ()
+
+        if key in self.cache_data:
+            self.queue.remove(key)
+
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            print("DISCARD:", self.queue[-1])
+            del self.cache_data[self.queue.pop(-1)]
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """If key is in cache returns item else None"""
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return (self.cache_data[key])
+
+        return (None)
