4c9a9bc25c5b595ede2eca03c22d4ca3dd0e418e
LeeWest89 <6683@holbertonstudents.com>
2024-05-07 20:17:06 +0000

    Might have to get help, I that it was suppost to be the oldest removed first but the example shows different, going to see what the checker does

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3
+"""Createing class LIFOCache"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Using LIFO to create a cache system"""
+    def __init__(self):
+        """Starting Cache"""
+        super().__init__()
+        self.queue = []
+        self.counter = 0
+
+    def put(self, key, item):
+        """Adding keys and items or overwriting items starting oldest"""
+        if key is None or item is None:
+            return ()
+
+        if key in self.cache_data and self.cache_data[key] != item:
+            self.cache_data[key] = item
+
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if self.queue:
+                    old_key = self.queue.pop(-1 - self.counter)
+                    print("DISCARD:", old_key)
+                    del self.cache_data[old_key]
+                    self.counter += 1
+
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """If key is in cache returns item else None"""
+        if key:
+            return (self.cache_data.get(key))
+
+        elif key not in self.cache_data:
+            return (None)
+
+        return (None)
