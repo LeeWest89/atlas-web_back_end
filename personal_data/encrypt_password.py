ce0f874a83cb7f8ff8e0d91d0a3fece0354ea263
LeeWest89 <6683@holbertonstudents.com>
2024-05-14 16:49:20 -0500

    After the issues with the database it was nice to find info on bcrypt and have a quick setup

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Encrypting Passwords"""
+
+
+import bcrypt
+
+
+def hash_password(password: str):
+    """encodes pasword using bcrypt"""
+    pswd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return (pswd)
