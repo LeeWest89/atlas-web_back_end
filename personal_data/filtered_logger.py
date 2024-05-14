2ac022441c305a375b47934c21632bd1fd171baf
LeeWest89 <6683@holbertonstudents.com>
2024-05-14 18:14:16 +0000

    finally got the info to redact, need to use msg instead of message

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -21,3 +22,23 @@ def filter_datum(fields: List[str], redaction: str,
     """
 
     return (hidden_message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """Stores list of fields"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """modifies the message useing filter_datum"""
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION, record.msg, self.SEPARATOR)
+        return (super().format(record))
