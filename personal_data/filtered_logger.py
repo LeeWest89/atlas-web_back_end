9c0dd8406781a6f540ef1d27029d5441d6c80b4b
LeeWest89 <6683@holbertonstudents.com>
2024-05-14 16:59:08 +0000

    fixed pycode for line 9

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Obfuscates the log depending on what needs to be redacted"""
     pattern = '(' + '|'.join(fields) + ')=[^' + re.escape(separator) + ']+'
     """
