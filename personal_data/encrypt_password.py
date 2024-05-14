44e6f1a35be9e8b8889c9b624143ec4358b73d2b
LeeWest89 <6683@holbertonstudents.com>
2024-05-14 17:04:20 -0500

    using checkpw to see if the passwords are the same or not

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """encodes pasword using bcrypt"""
     pswd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return (pswd)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if password and hashed_password are the same"""
+    return (bcrypt.checkpw(password.encode('utf-8'), hashed_password))
