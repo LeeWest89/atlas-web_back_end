26ec201ac1b59ea7abf037f99e35781ed2afdcab
LeeWest89 <6683@holbertonstudents.com>
2024-05-15 18:16:44 +0000

    checking authorization between path and excluded_paths, learned about slice notation

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,22 @@ from typing import List, TypeVar
 class Auth:
     """Class for Authorization"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Public Method for authorization"""
-        return (False)
+        """Public Method for authorization of a path"""
+        if not (path and excluded_paths):
+            return (True)
+
+        if path.endswith('/'):
+            return (False)
+        else:
+            path += '/'
+
+        for e_path in excluded_paths:
+            if e_path.endswith('/'):
+                e_path = e_path[:-1]
+            if path.startswith(e_path):
+                return (False)
+
+        return (True)
 
     def authorization_header(self, request=None) -> str:
         """Public Method for authorization"""
