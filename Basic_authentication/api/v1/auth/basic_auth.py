01817a08d882dd2eb456178aabcfe25fb914fa32
LeeWest89 <6683@holbertonstudents.com>
2024-05-15 21:30:23 +0000

    Checks if user exist if the email does checks for valid password and returns the user

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -57,3 +59,25 @@ class BasicAuth(Auth):
 
         email, pswd = decoded_base64_authorization_header.split(':', 1)
         return (email, pswd)
+
+    def user_object_from_credentials(
+                                     self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns user instance"""
+
+        if user_email is None or type(user_email) != str:
+            return (None)
+
+        if user_pwd is None or type(user_pwd) != str:
+            return (None)
+
+        user = User.search({'email': user_email})
+
+        if not user:
+            return (None)
+
+        if not user[0].is_valid_password(user_pwd):
+            return (None)
+
+        return (user[0])
