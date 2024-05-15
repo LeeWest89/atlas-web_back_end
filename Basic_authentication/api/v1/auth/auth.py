72ba1a766915db1712b3454fdf444f4f550570b2
LeeWest89 <6683@holbertonstudents.com>
2024-05-15 17:42:52 +0000

    setting up auth.py

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Authorization for the api
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public Method for authorization"""
+        return (False)
+
+    def authorization_header(self, request=None) -> str:
+        """Public Method for authorization"""
+        return (None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Public Method for authorization"""
+        return (None)
