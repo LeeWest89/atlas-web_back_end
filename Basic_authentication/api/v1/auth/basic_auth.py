ae741f739bea9e062dbe6689bb8a2d61c2fe0d68
LeeWest89 <6683@holbertonstudents.com>
2024-05-15 19:57:02 +0000

    figured out I could use split witht he delimiter set to space

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class, inherting from Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Finds the Base64 in Authorization"""
+        if authorization_header is None:
+            return (None)
+
+        if type(authorization_header) != str:
+            return(None)
+
+        if not authorization_header.startswith('Basic '):
+            return(None)
+
+        return (authorization_header.split(' ')[1])
