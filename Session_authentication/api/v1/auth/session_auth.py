ddfbe626b49619bf0b42d1eea72e0f98aa236d51
LeeWest89 <6683@holbertonstudents.com>
2024-05-21 17:02:56 +0000

    getting user id by using get(session_id)

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,10 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return (session_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Getting user_id by using session_id"""
+        if type(session_id) != str or session_id is None:
+            return (None)
+
+        return (self.user_id_by_session_id.get(session_id))
