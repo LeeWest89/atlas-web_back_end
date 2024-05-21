dd3fa20c1a4b3829df579379a4e4ed4715e1212f
LeeWest89 <6683@holbertonstudents.com>
2024-05-21 16:35:53 +0000

    creating a unique session_id then tying it the the user_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,11 +4,18 @@
 
 
 from api.v1.auth.auth import Auth
-import base64
-from typing import TypeVar
-from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class, inherting from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if type(user_id) != str or user_id is None:
+            return (None)
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return(session_id)
