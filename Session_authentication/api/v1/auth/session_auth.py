a4482ed098a82c354f9ea1a77570cd09321a4587
LeeWest89 <6683@holbertonstudents.com>
2024-05-21 16:40:05 +0000

    everything is now documented and pycodestyle has been checked

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -12,10 +12,11 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """creating a unique session_id then tying it the the user_id"""
         if type(user_id) != str or user_id is None:
             return (None)
 
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
-        return(session_id)
+        return (session_id)
