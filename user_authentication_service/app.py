78b6f24938bdbdb846d6c2bcb23ea9a17c9ef255
LeeWest89 <6683@holbertonstudents.com>
2024-05-22 17:43:29 +0000

    very simple setup, similar to things from basic and session auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -4,15 +4,32 @@ Route module
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def message():
+    """return a JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Returns jsonified email and message or a VauleError message"""
+    email = request.form.get("email")
+    pswd = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, pswd)
+
+        return (jsonify({"email": user.email, "message": "user created"}), 200)
+    
+    except ValueError:
+        return (jsonify({"message": "email already registered"}), 400)
 
 
 if __name__ == "__main__":
