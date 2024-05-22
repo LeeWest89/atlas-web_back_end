3f871ac055542b01bbf200fecffa70bf5d759255
LeeWest89 <6683@holbertonstudents.com>
2024-05-22 05:03:22 +0000

    Put the if statement back, but in the right spot

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -68,6 +68,9 @@ def before_handler():
                auth.session_cookie(request) is None):
                 abort(401)
 
+            if auth.current_user(request) is None:
+                abort(403)
+
             request.current_user = auth.current_user(request)
 
 
