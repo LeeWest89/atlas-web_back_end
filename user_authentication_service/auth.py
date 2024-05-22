3f1538225f0054bb313973860a7872704c4c89c4
LeeWest89 <6683@holbertonstudents.com>
2024-05-22 17:05:29 +0000

    NoResultsFound was causes issues so I had to bring it in and bypass it with an except

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,8 +4,32 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes the password"""
     return (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Sets up new users"""
+        try:
+            current_user = self._db.find_user_by(email=email)
+
+            if current_user:
+                raise ValueError("User {} already exists".format(email))
+
+        except NoResultFound:
+            pass
+
+        return (self._db.add_user(email, _hash_password(password)))
