d4c76a4612923401580d23d7baa3eb11db2676b5
LeeWest89 <6683@holbertonstudents.com>
2024-05-22 23:20:14 +0000

    everything seems to be setup right

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -83,5 +83,20 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Creates reset token for passwords"""
+    email = request.form.get('email')
+
+    try:
+        user = AUTH.find_user_by(email)
+
+    except ValueError:
+        abort(403)
+
+    reset_token = AUTH.get_reset_password_token(email)
+    return (jsonify({"email": email, "reset_token": reset_token}), 200)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
