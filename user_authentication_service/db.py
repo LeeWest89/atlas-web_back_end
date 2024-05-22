bc8c407b6036db2d6caf0371527d73c2136d12e9
LeeWest89 <6683@holbertonstudents.com>
2024-05-22 16:29:17 +0000

    using hasattr and setattr to set new values

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -49,3 +49,15 @@ class DB:
 
         except InvalidRequestError:
             raise InvalidRequestError()
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user"""
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError()
+
+        self._session.commit()
