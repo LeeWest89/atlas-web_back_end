9923970c60502ea2d42a2167ca56c81653bf5363
LeeWest89 <6683@holbertonstudents.com>
2024-05-22 16:03:36 +0000

    using try and except more try to get use to it, feturning user or correct exceptions according to grey box

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -36,3 +37,16 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return (user)
+
+    def find_user_by(self, **kwargs) -> User:
+        """Use keyword to find user"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+
+            return (user)
+
+        except NoResultFound:
+            raise NoResultFound()
+        
+        except InvalidRequestError:
+            raise InvalidRequestError()
