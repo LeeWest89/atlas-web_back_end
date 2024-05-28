b408c7913bf09e27c69f48e3a0cf36434e8760ad
LeeWest89 <6683@holbertonstudents.com>
2024-05-28 18:15:34 +0000

    test is running well I think I did everything correctly

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,24 @@
-#include
+#!/usr/bin/env python3
+"""Unitest for utlis.py"""
+
+
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test for GithubOrgClient"""
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json', return_value={})
+    def test_org(self, org_name, mock):
+        """test that GithubOrgClient.org returns the correct value"""
+        self.assertEqual(GithubOrgClient(org_name).org, {})
+
+
+if __name__ == '__main__':
+    unittest.main()
