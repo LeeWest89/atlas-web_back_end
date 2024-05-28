6529545804e09aa51cb9195f45e1b887869df294
LeeWest89 <6683@holbertonstudents.com>
2024-05-28 16:27:41 +0000

    forgot to pycode

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -18,13 +18,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """test that the method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Use the assertRaises context manager to test that a KeyError is raised"""
+        """Use the assertRaises context manager to
+        test that a KeyError is raised"""
         with self.assertRaises(KeyError) as correct_message:
             access_nested_map(nested_map, path)
 
