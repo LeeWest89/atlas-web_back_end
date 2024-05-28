e06cfade2bbdfca370d52e05df41dd0a142f6ec7
LeeWest89 <6683@holbertonstudents.com>
2024-05-28 16:22:46 +0000

    I'm over thinking unittesting

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -19,5 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Use the assertRaises context manager to test that a KeyError is raised"""
+        with self.assertRaises(KeyError) as correct_message:
+            access_nested_map(nested_map, path)
+
+
 if __name__ == "__main__":
     unittest.main()
