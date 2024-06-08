a401305b7b9bdaba72072f47d28930a6295cc7e1
LeeWest89 <6683@holbertonstudents.com>
2024-06-08 17:33:59 +0000

    Think I setup the get method correctly, also added a decode method that decodes bytes to str

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,3 +19,31 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    bytes,
+                                                                    int,
+                                                                    float,
+                                                                    None]:
+        """gets data and can convert the data back to the desired format."""
+        data = self._redis.get(key)
+
+        if data is None:
+            return (None)
+
+        if not fn:
+            return (data)
+        else:
+            return fn(data)
+
+    def decode(self, data: bytes) -> str:
+        """Decode UTF-8 string."""
+        return (data.decode('utf-8'))
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Gets str from redis"""
+        return (self.get(key, self.decode))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Gets int from redis"""
+        return (self.get(key, int))
