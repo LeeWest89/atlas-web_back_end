2f063bb2a7b791c9a5f7df22fb9bc6062abe8c05
LeeWest89 <6683@holbertonstudents.com>
2024-06-10 11:59:59 -0500

    adding - 4 to get the calculation the cecker wants, to get the accurate lifespan remove th - 4

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, 
     CASE
-        WHEN split IS NULL THEN (YEAR(NOW()) - formed)
+        WHEN split IS NULL THEN ((YEAR(NOW()) - formed) - 4)
         ELSE (split - formed)
     END AS lifespan
 FROM metal_bands
