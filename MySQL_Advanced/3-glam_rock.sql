c92163d91ae1a385fcae0007adc2d90ded67165a
LeeWest89 <6683@holbertonstudents.com>
2024-06-10 11:52:59 -0500

    had issues figuring out the syntax for the calculating NULL

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,1 +1,9 @@
-#include
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, 
+    CASE
+        WHEN split IS NULL THEN (YEAR(NOW()) - formed)
+        ELSE (split - formed)
+    END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
