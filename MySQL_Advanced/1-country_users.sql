c42827c3ebb8b53fc439c9468c8c4fe78887c0c8
LeeWest89 <6683@holbertonstudents.com>
2024-06-10 10:57:34 -0500

    adding country column to users table

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,1 +1,12 @@
-#include
+-- creates a table users now with country data
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country VARCHAR(2) NOT NULL DEFAULT 'US',
+    CONSTRAINT country_check CHECK (country IN ('US', 'CO', 'TN'))
+);
+
+ALTER TABLE users
+ADD COLUMN country VARCHAR(2) NOT NULL DEFAULT 'US',
+ADD CONSTRAINT country_check CHECK (country IN ('US', 'CO', 'TN'));
