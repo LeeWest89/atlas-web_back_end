0cb457add61ad55e925bef2b574bb5d6ae88ab88
LeeWest89 <6683@holbertonstudents.com>
2024-06-11 01:56:22 -0500

    Have it workin, I knew there was a update_many,

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,14 @@
-#include
+#!/usr/bin/env python3
+"""changes all topics of a school document based on the name"""
+
+
+from pymongo.collection import Collection
+from typing import List
+
+
+def update_topics(mongo_collection: Collection, name: str, topics: List[str]) -> None:
+    """Changing topics"""
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
