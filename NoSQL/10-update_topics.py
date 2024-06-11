6443e289acffb2fddbd7e1ed0fece8bc497b6c81
LeeWest89 <6683@holbertonstudents.com>
2024-06-11 02:07:32 -0500

    Think I fixed the documentation error and have first version of 11 ready to push

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,11 +2,7 @@
 """changes all topics of a school document based on the name"""
 
 
-from pymongo.collection import Collection
-from typing import List
-
-
-def update_topics(mongo_collection: Collection, name: str, topics: List[str]) -> None:
+def update_topics(mongo_collection, name, topics):
     """Changing topics"""
     mongo_collection.update_many(
         {'name': name},
