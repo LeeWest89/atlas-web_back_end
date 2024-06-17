b8b0e22dba68cd5a3e6de105fa59b1457de4c0e1
LeeWest89 <6683@holbertonstudents.com>
2024-06-17 06:34:58 -0500

    basically using 2 but with promises and some slight modifications

---
+++ Node_JS/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n');
+      const studentNames = {};
+      let students = 0;
+
+      for (let i = 1; i < lines.length; i += 1) {
+        const line = lines[i].trim();
+
+        if (line) {
+          const [firstname, lastname, age, field] = line.split(',');
+
+          if (firstname && lastname && age && field) {
+            if (!studentNames[field]) {
+              studentNames[field] = [];
+            }
+            studentNames[field].push(firstname);
+            students += 1;
+          }
+        }
+      }
+
+      console.log(`Number of students: ${students}`);
+      for (const [field, firstnames] of Object.entries(studentNames)) {
+        console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
