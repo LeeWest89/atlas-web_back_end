933ad8d0fe4e72f15b176dd032f09a4fffd7c179
LeeWest89 <6683@holbertonstudents.com>
2024-06-17 06:21:33 -0500

    correcting ESlint

--- Node_JS/1-stdin.js
+++ Node_JS/1-stdin.js
@@ -1,22 +1,21 @@
 // Program that excutes through command line
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-    const word = data.trim();
+  const word = data.trim();
 
-    if (word.toLowerCase() === 'end' || word.toLowerCase() === 'exit' || word.toLowerCase() === 'close') {
-        process.stdout.write('This important software is now closing\n')
-        process.exit()
-    }
-    else if(word === ''){
-        process.stdout.write('Please enter your name\n')
-    } else {
-        process.stdout.write(`Your name is: ${word}\n`);
-    }
+  if (word.toLowerCase() === 'end' || word.toLowerCase() === 'exit' || word.toLowerCase() === 'close') {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  } else if (word === '') {
+    process.stdout.write('Please enter your name\n');
+  } else {
+    process.stdout.write(`Your name is: ${word}\n`);
+  }
 });
 
 process.on('SIGINT', () => {
-    process.stdout.write('This important software is now closing\n')
-    process.exit()
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -1,43 +1,39 @@
 // function to attemp to read database
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
-    let lines
-    try {
-        lines = fs.readFileSync(path);
-    }
+  let lines;
+  try {
+    lines = fs.readFileSync(path);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 
-    catch (error) {
-        throw new Error('Cannot load the database');
-    }
+  lines = fs.readFileSync(path, 'utf8').split('\n');
+  const studentNames = {};
+  let students = 0;
+  let i = 1;
 
-    lines = fs.readFileSync(path, 'utf8').split('\n');
-    const student_names = {}
-    let students = 0;
-    let i = 1;
+  for (; i < lines.length; i += 1) {
+    const line = lines[i];
 
+    if (line.trim()) {
+      const [firstname, lastname, age, field] = line.split(',');
 
-    for (; i < lines.length; i++) {
-
-        const line = lines[i];
-
-        if (line.trim()) {
-            const [firstname, lastname, age, field] = line.split(',');
-
-            if (firstname && lastname && age && field) {
-                if (!student_names[field]) {
-                    student_names[field] = [];
-                }
-                student_names[field].push(firstname);
-                students += 1;
-            }
+      if (firstname && lastname && age && field) {
+        if (!studentNames[field]) {
+          studentNames[field] = [];
         }
+        studentNames[field].push(firstname);
+        students += 1;
+      }
     }
+  }
 
-    console.log(`Number of students: ${students}`)
-    for (const [field, firstnames] of Object.entries(student_names)) {
-        console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
-    }
+  console.log(`Number of students: ${students}`);
+  for (const [field, firstnames] of Object.entries(studentNames)) {
+    console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
+  }
 }
 
 module.exports = countStudents;
