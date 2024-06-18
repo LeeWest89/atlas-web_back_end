df34da35780b3e8e8de8f8c83137a5f11adf7017
LeeWest89 <6683@holbertonstudents.com>
2024-06-18 02:17:04 -0500

    Working on full server

--- Node_JS/full_server/controllers/AppController.js
+++ Node_JS/full_server/controllers/AppController.js
@@ -1,1 +1,8 @@
-#include
+// creates AppController class with static method getHomepage
+class AppController {
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
+  }
+}
+
+module.export = AppController;
--- Node_JS/full_server/controllers/StudentsController.js
+++ Node_JS/full_server/controllers/StudentsController.js
@@ -1,1 +1,51 @@
-#include
+// creates StudentsController class and static methods getAllstudents and getAllStudentsByMajor
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    const dbPath = '../../database.csv';
+
+    readDatabase(dbPath)
+      .then((studentNames) => {
+        response.status(200);
+        let infoOutput = 'This is the list of our students\n';
+        let students = 0;
+        const fields = Object.keys(studentNames);
+
+        fields.forEach((field) => {
+          students += studentNames[field].length;
+        });
+
+        infoOutput += `Number of students: ${students}\n`;
+
+        const sortedFields = fields.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+        sortedFields.forEach((field) => {
+          infoOutput += `Number of students in ${field}: ${studentNames[field].length}. List: ${studentNames[field].join(', ')}\n`;
+        });
+
+        response.send(200, infoOutput);
+      })
+
+      .catch(() => response.send(500, 'Cannot load the database'));
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const major = request.query.field;
+    const dbPath = '../../database.csv';
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(dbPath)
+        .then((studentNames) => {
+          const students = studentNames[major];
+
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => response.send(500, 'Cannot load the database'));
+    }
+  }
+}
+
+module.export = StudentsController;
