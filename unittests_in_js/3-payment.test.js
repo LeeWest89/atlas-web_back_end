ee05cf8f13f74351c1a8e49a61859ce28b0584b9
LeeWest89 <6683@holbertonstudents.com>
2024-06-21 15:28:00 -0500

    Had a small error in 3-payment utlis instead of utils

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,1 +1,16 @@
-#include
+// test for 3-payment.js
+const sinon = require('sinon');
+const assert = require('assert');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber', () => {
+    const calSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(calSpy);
+    sinon.assert.calledOnceWithExactly(calSpy, 'SUM', 100, 20);
+    calSpy.restore();
+  });
+});
