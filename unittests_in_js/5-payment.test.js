55d4839dcd75c8de316c9d2488832c3ed7c9a807
LeeWest89 <6683@holbertonstudents.com>
2024-06-21 16:16:58 -0500

    Hook created

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,1 +1,27 @@
-#include
+// test for 5-payment.js
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let conSpy;
+
+  beforeEach(() => {
+    conSpy = sinon.spy(console, 'log')
+  });
+
+  afterEach(() => {
+    conSpy.restore();
+  });
+
+  it('should log "The total is: 120"', () => {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(conSpy);
+    sinon.assert.calledWithExactly(conSpy, 'The total is: 120');
+  });
+
+  it('should log "The total is: 20"', () => {
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledOnce(conSpy);
+    sinon.assert.calledWithExactly(conSpy, 'The total is: 20');
+  });
+});
