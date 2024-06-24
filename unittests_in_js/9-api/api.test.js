9c50a56ffc1d06cf4bff3d30870c650c6a2e24ea
LeeWest89 <6683@holbertonstudents.com>
2024-06-24 03:03:36 -0500

    added endpoint cart and three test

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,1 +1,60 @@
-#include
+// uniittest for api.js
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index Testing', () => {
+  describe('GET /', () => {
+    it('Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(options, function (error, response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/12', () => {
+    it('has correct code and id', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/12',
+        method: 'GET',
+      };
+      request(options, function (error, response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/hello', () => {
+    it('Has status 404', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/hello',
+        method: 'GET',
+      };
+      request(options, function (error, response) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/', () => {
+    it('Has status 404', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/',
+        method: 'GET',
+      };
+      request(options, function (error, response) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+});
