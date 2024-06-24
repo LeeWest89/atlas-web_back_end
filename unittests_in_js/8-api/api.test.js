7e6c64261b77bc686aed1519f8b36e786486cb56
LeeWest89 <6683@holbertonstudents.com>
2024-06-24 02:35:57 -0500

    Finished 8, learned about options

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,1 +1,20 @@
-#include
+// uniittest for api.js
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index Testing', () => {
+  describe('GET /', () => {
+    it('Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(options, function (error, response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+});
