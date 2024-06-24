63d6401c4d0da661743e26cf18d6291317602a59
LeeWest89 <6683@holbertonstudents.com>
2024-06-24 04:48:24 -0500

    1 console.logs when redis is connected or has an error

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+// connect to the Redis server running on your machine
+import redis from 'redis';
+
+redis.createClient().on('connect', () => {
+  console.log('Redis client connected to the server')
+});
+
+redis.createClient().on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`)
+});
