f28775d8a1a7267da62b26315bcb8b45c131d8fd
LeeWest89 <6683@holbertonstudents.com>
2024-06-25 12:11:37 -0500

    Able to create jobs in a queue

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+// Creating a job
+
+import kue from 'kue';
+
+const que = kue.createQueue();
+
+const jobInfo = {
+  phoneNumber: 'string1',
+  message: 'string2',
+};
+
+const job = que.create('push_notification_code', jobInfo).save((error) => {
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`)
+  }
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () =>{
+  console.log('Notification job failed')
+});
