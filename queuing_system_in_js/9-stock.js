1d5e2fa25dcd23a6b80427e952c658b6186ced1b
LeeWest89 <6683@holbertonstudents.com>
2024-06-26 13:11:15 -0500

    Making progress on 9-stock I'm up to Product details

---
+++ queuing_system_in_js/9-stock.js
@@ -0,0 +1,54 @@
+// function to check item stock
+
+const express = require('express');
+const redis = require('redis');
+const { promisify } = require('util');
+
+const listProducts = [
+  {itemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 4},
+  {itemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10},
+  {itemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2},
+  {itemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5},
+];
+const app = express();
+const port = 1245;
+const client = redis.createClient();
+const getA = promisify(client.get).bind(client);
+const getS = promisify(client.set).bind(client);
+
+function getItemById(id) {
+  return (listProducts.find(product => product.itemId === id));
+}
+
+function reserveStockById(itemId, stock) {
+  client.set(`item.${itemId}`,stock.toString());
+}
+
+async function getCurrentReservedStockById(itemId) {
+  const reserved = await getA(`item.${itemId}`);
+  return (parseInt(reserved) || 0);
+}
+
+app.get('/list_products', (req, res) => {
+  res.json(listProducts);
+});
+
+app.get('/list_products/:itemId', async (req, res) => {
+  const itemId = parseInt(req.params.itemId);
+  const item = getItemById(itemId);
+
+  if (!item) {
+    res.json({ status: "Product not found" });
+    return;
+  }
+
+  const info = {
+    ...item,
+    currentQuantity: await getCurrentReservedStockById(itemId)
+  };
+
+  res.json(info);
+});
+
+
+app.listen(port, () => {});
